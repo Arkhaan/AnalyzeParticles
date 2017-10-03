30f6661dd5fabd46aeb9cf07f96425a8d453b5db
JeanMainguy <jean.mainguy@outlook.fr>
2017-10-03 23:34:25 +0200

    Update Benchmark.js

--- Benchmark.js
+++ Benchmark.js
@@ -1,20 +1,33 @@
-// import ij.*;
-// import ij.plugin.filter.PlugInFilter;
-// import ij.process.*;
-// import ij.gui.*;
-// import java.awt.*;
-// import java.awt.event.*;
-// import java.util.*;
-
-    var parametre;
-    parametre = "area bounding fit";
-    var d = new Date();
-    var time = d.getTime();
-    IJ.log(time);
-
-    imp = IJ.openImage("http://wsr.imagej.net/images/particles.gif");
+function run_AnalyzeP(parametre, nombre, imp) {
     IJ.run("Set Measurements...", parametre + " redirect=None decimal=3");
-    IJ.run(imp, "Analyze Particles...", "display");
+    result = new Array();
+    var i;
+    var total = 0;
+    for (i = 0; i < nombre; i++) {
+        var start = new Date();
+        var start_time = start.getTime();
+       
+        IJ.run(imp, "Analyze Particles...", "display");
+        
+        var end = new Date();
+        var end_time = end.getTime();
+        var time = end_time - start_time;
+        total = total + time; 
+        
+    }
+    
+    return time/nombre
+}
 
-    time = d.getTime();
-    IJ.log(time);
+
+var setM = new Array("area", "mean", "standard", "modal", "min", "centroid", "center", "perimeter", "bounding", "fit", "shape", "feret's", "integrated", "skewness", "area_fraction");
+
+imp = IJ.openImage("http://wsr.imagej.net/images/particles.gif");
+
+var i;
+var nb = 100;
+for (i = 0; i < setM.length; i++) {
+    result = run_AnalyzeP(setM[i], nb, imp);
+    IJ.log(setM[i])
+    IJ.log(result)  
+}
